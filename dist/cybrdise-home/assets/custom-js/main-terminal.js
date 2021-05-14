68e8078c1988f9cd7f26bf9387eddbde3000dff4
Pranav Kulkarni <pranavkulkarni195@gmail.com>
2021-05-14 17:58:47 +0530

    Implemented Winbox.js clean way

--- dist/cybrdise-home/assets/custom-js/main-terminal.js
+++ dist/cybrdise-home/assets/custom-js/main-terminal.js
@@ -6,21 +6,21 @@ const contactContent = document.querySelector('#contactContent')
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
     html: ` 
-    <h2 style="color: #00aa00;">about-me:$
+    <h2 style="color: #00aa00; margin-bottom: 20px;">about-me:$
       <span class="cursor">_</span>
     </h2>
     <p>Hi! I am an <span style="color: #0F94D2;">Arch</span> user btw.</p>
     
     <p style="font-weight: bold;">Check out some of my Community Loved Projects!</p>
-    <ol style="font-weight: bold; text-decoration: none; color: #00aa00;">  
-      <li href="https://github.com/thegamerhat/quick-vm/">Quick VM</li>
-      <li href="https://github.com/thegamerhat/assistant">Linux Assistant</li>
-      <li href="https://github.com/thegamerhat/appbook">AppBook</li>
-      <li href="https://github.com/thegamerhat/windows-9.5">Windows 9.5</li>
-      <li href="https://github.com/thegamerhat/vs-code-mobile">VS Code Mobile</li>
-    </ol>
-    <div>
-      <li>Donate: <a href="https://paypal.me/gamerhat18">PayPal</a></li>
+    <div style="font-weight: bold; text-decoration: none; color: #00aa00;">  
+      <ol>
+        <li href="https://github.com/thegamerhat/quick-vm/">Quick VM</li>
+        <li href="https://github.com/thegamerhat/assistant">Linux Assistant</li>
+        <li href="https://github.com/thegamerhat/appbook">AppBook</li>
+        <li href="https://github.com/thegamerhat/windows-9.5">Windows 9.5</li>
+        <li href="https://github.com/thegamerhat/vs-code-mobile">VS Code Mobile</li>
+      </ol>
+      <p>Donate: <a href="https://paypal.me/gamerhat18">PayPal</a></p>
     </div>
 
     `
@@ -29,8 +29,8 @@ about.addEventListener('click', () => {
     title: 'About',
     background: '#2A2A2A',
     // modal: true,
-    width: '430px',
-    height: '430px',
+    width: '352px',
+    height: '444px',
     top: 0,
     right: 0,
     bottom: 0,
@@ -48,23 +48,22 @@ about.addEventListener('click', () => {
 contact.addEventListener('click', () => {
   const contactBox = new WinBox({
     html: `
-    <h2 style="color: #00aa00;">contact-me:$
+    <h2 style="color: #00aa00; margin-bottom: 30px;">contact-me:$
       <span class="cursor">_</span>
     </h2>
-    <p>Social Links: </p>
-    <ul>
-      <li>Email: <a href="mailto:gamerhat18@protonmail.com">gamerhat18@protonmail.com</a></li>
-      <li>Telegram: <a href="https://t.me/thegamerhat">thegamerhat</a></li>
-      <li>GitHub: <a href="https://github.com/thegamerhat">thegamerhat</a></li>
-      <li>Discord: @gamerhat#2074s</li>
-      <li>Twitter: <a href="https://twitter.com/thegamerhat">thegamerhat</a></li>
-      <li>Instagram: <a href="https://instagram.com/thegamerhat">thegamerhat</a></li>
-    </ul>`,
+    <div>
+      <p>Email: <a href="mailto:gamerhat18@protonmail.com">gamerhat18@pm.me</a></p>
+      <p>Telegram: <a href="https://t.me/thegamerhat">thegamerhat</a></p>
+      <p>GitHub: <a href="https://github.com/thegamerhat">thegamerhat</a></p>
+      <p>Discord: @gamerhat#2074</p>
+      <p>Twitter: <a href="https://twitter.com/thegamerhat">thegamerhat</a></p>
+      <p>Instagram: <a href="https://instagram.com/thegamerhat">thegamerhat</a></p>
+    </div>`,
     class: ["no-full"],
     title: 'Contact',
     background: '#2A2A2A',
-    width: '430px',
-    height: '380px',
+    width: '352px',
+    height: '420px',
     top: 0,
     right: 0,
     bottom: 0,
