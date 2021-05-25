b66e199a9b8ffaf50b565a4292287c0d6d08ddae
Pranav Kulkarni <pranavkulkarni195@gmail.com>
2021-05-25 18:02:49 +0530

    added

--- src/app/MyComponents/homepage-content/Project.ts
+++ src/app/MyComponents/homepage-content/Project.ts
@@ -2,6 +2,7 @@ export class Project {
     id: number
     name: string
     logo: string
+    logoWidth: number
     description: string
     projectPageLink: string
     gitLink: string
--- src/app/MyComponents/homepage-content/homepage-content.component.ts
+++ src/app/MyComponents/homepage-content/homepage-content.component.ts
@@ -14,13 +14,24 @@ export class HomepageContentComponent implements OnInit {
     this.projects = [
       {
        id: 1,
-       name: 'Quick VM',
-       logo: '../assets/vm-logo.svg',
-       description: 'Make a Windows VM in under 100 seconds!',
+       name: 'Prism',
+       logo: '../assets/Prism.svg',
+       logoWidth: 100,
+       description: 'The ultimate Containerization for Proprietary software!',
        projectPageLink: '#',
-       gitLink: 'https://github.com/thegamerhat/quick-vm/',
-       blogLink: 'https://blog.cybrdise.com/posts/quick-vm/',
+       gitLink: 'https://github.com/thegamerhat/',
+       blogLink: 'https://blog.cybrdise.com/posts/',
       },
+      {
+        id: 2,
+        name: 'Quick VM',
+        logo: '../assets/vm-logo.svg',
+        logoWidth: 80,
+        description: 'Make a Windows VM in under 100 seconds!',
+        projectPageLink: '#',
+        gitLink: 'https://github.com/thegamerhat/quick-vm/',
+        blogLink: 'https://blog.cybrdise.com/posts/quick-vm/',
+       },
   ];
   }
 
